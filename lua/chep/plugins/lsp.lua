ad959b1dc27e79e2058dc11a6577ebe9ff467b3a
Rafael Vieira <62526496+rafaaahype@users.noreply.github.com>
2026-04-28 09:56:12 -0300

    v1.1 nvim chep

--- lua/chep/plugins/lsp.lua
+++ lua/chep/plugins/lsp.lua
@@ -6,15 +6,15 @@ return {
       "hrsh7th/cmp-nvim-lsp",
     },
     config = function()
-      -- 1. Setup do Mason básico
+      -- setup do mason
       require("mason").setup()
       
-      -- 2. Setup do Mason-Lspconfig (garante a instalação)
+      -- setup mason-lsp
       require("mason-lspconfig").setup({
         ensure_installed = { "clangd", "html", "cssls", "ts_ls", "emmet_ls" },
       })
 
-      -- 3. Configuração dos servidores (Jeito clássico mas compatível)
+      -- config servers
       local lspconfig = require("lspconfig")
       local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
@@ -26,7 +26,7 @@ return {
         })
       end
 
-      -- Atalhos
+      -- atalhos
       vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
       vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
     end
