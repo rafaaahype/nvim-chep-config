669a0f25e2db64da6be70ab279bcd3c31787324c
rafaelchep <rafaelsvieira777@gmail.com>
2026-05-08 10:00:30 -0300

    resolvido - problema lualine nao encontrando catpuccin

--- lua/chep/plugins/ui.lua
+++ lua/chep/plugins/ui.lua
@@ -1,46 +1,78 @@
 return {
   {
     "nvim-tree/nvim-web-devicons",
+
     config = function()
-      require("nvim-web-devicons").setup({ default = true })
-    end
+      require("nvim-web-devicons").setup({
+        default = true,
+      })
+    end,
   },
 
   {
     "sunjon/stylish.nvim",
+
     lazy = false,
     priority = 1000,
+
     config = function()
       require("stylish").setup()
-    end
+    end,
   },
 
   {
     "catppuccin/nvim",
+
     name = "catppuccin",
+    lazy = false,
     priority = 1000,
+
     config = function()
-      require("catppuccin").setup({ transparent_background = true })
+      require("catppuccin").setup({
+        transparent_background = true,
+
+        integrations = {
+          lualine = true,
+          bufferline = true,
+        },
+      })
+
       vim.cmd.colorscheme("catppuccin")
 
       vim.cmd([[highlight IndentBlanklineIndent guifg=#313244]])
       vim.cmd([[highlight IndentBlanklineScope guifg=#f5c2e7]])
-    end
+    end,
   },
 
   {
     "nvim-lualine/lualine.nvim",
+
+    dependencies = {
+      "nvim-tree/nvim-web-devicons",
+      "catppuccin/nvim",
+    },
+
     config = function()
-      require("lualine").setup({ options = { theme = "catppuccin" } })
-    end
+      require("lualine").setup({
+        options = {
+          theme = "auto",
+        },
+      })
+    end,
   },
 
   {
     "akinsho/bufferline.nvim",
+
+    dependencies = {
+      "nvim-tree/nvim-web-devicons",
+    },
+
     config = function()
       require("bufferline").setup({})
+
       vim.keymap.set("n", "<Tab>", ":BufferLineCycleNext<CR>")
       vim.keymap.set("n", "<S-Tab>", ":BufferLineCyclePrev<CR>")
-    end
+    end,
   },
 }
