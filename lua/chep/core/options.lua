88b4bccab1991385ac652806e675605b90ff76a6
rafael <rafaelsvieira777@gmail.com>
2026-08-23 10:03:33 -0300

    feat: new cursor config added

--- lua/chep/core/options.lua
+++ lua/chep/core/options.lua
@@ -7,6 +7,8 @@ vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 vim.opt.softtabstop = 4
 
+vim.opt.guicursor = "n-v-c-i-ci-ve:block-blinkwait175-blinkoff150-blinkon175"
+
 vim.g.live_server = {
   port = 5500,
   browser = nil,
